a2a7a8a28e8f113a762a4d1043e47084a0626a57
vietstack <vietstack@gmail.com>
2014-10-17 00:04:47 +0700

    thay pass

--- 3-juno-aio-creatusetenant.sh
+++ 3-juno-aio-creatusetenant.sh
@@ -121,7 +121,7 @@ keystone endpoint-create \
 sleep 5
 echo "########## TAO FILE CHO BIEN MOI TRUONG ##########"
 echo "export OS_USERNAME=admin" > admin-openrc.sh
-echo "export OS_PASSWORD=Welcome123" >> admin-openrc.sh
+echo "export OS_PASSWORD=OpenStack123" >> admin-openrc.sh
 echo "export OS_TENANT_NAME=admin" >> admin-openrc.sh
 echo "export OS_AUTH_URL=http://$MASTER:35357/v2.0" >> admin-openrc.sh
 
