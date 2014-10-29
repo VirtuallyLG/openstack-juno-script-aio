26609f310188665429cb751020da9a8a599028fc
Hieu LE <hieulq19@gmail.com>
2014-10-29 17:35:12 +0700

    Update create-network.sh

--- create-network.sh
+++ create-network.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-echo "########## Khai bao rule cho policy ##########"
+echo "########## SETUP POLICY RULES ##########"
 
 nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
 nova secgroup-add-rule default tcp 1 65535 0.0.0.0/0
@@ -10,11 +10,11 @@ nova secgroup-add-rule default udp 1 65535 0.0.0.0/0
 # PROVIDER
 #############
 
-echo "########## Tao network cho provider (EXTENAL) ##########"
+echo "########## CREATE NETWORK FOR PROVIDER (EXTENAL) ##########"
 sleep 3
 neutron net-create ext_net --router:external True --shared 
 
-echo "########## Tao subnet cho EXTENAL ##########"
+echo "########## CREATE SUBNET EXTENAL ##########"
 sleep 3
 neutron subnet-create --name sub_ext_net ext_net 192.168.1.0/24 --gateway 192.168.1.1 --allocation-pool start=192.168.1.150,end=192.168.1.220 --enable_dhcp=False --dns-nameservers list=true 8.8.8.8 8.8.4.4 210.245.0.11
 
@@ -23,28 +23,28 @@ neutron subnet-create --name sub_ext_net ext_net 192.168.1.0/24 --gateway 192.16
 # Network cho tenant
 ####################
 
-echo "########## Tao network cho tenant ##########"
+echo "########## CREATE NETWORK FOR TENANT ##########"
 sleep 3
 neutron net-create int_net 
 
-echo "########## Tao subnet cho network trong tenant ##########"
+echo "########## CREATE SUBNET FOR NETWORK IN TENANT ##########"
 sleep 3
 neutron subnet-create int_net --name int_subnet --dns-nameserver 8.8.8.8 172.16.10.0/24
 
 
 #####################
-# Tao router, gan network, gan interface 
+# CREATE ROUTER, ASSIGN NETWORK, INTERFACE
 #####################
 
-echo "########## Tao router ##########"
+echo "########## CREATE ROUTER ##########"
 sleep 3
 neutron router-create router_1
 
-echo "########## Thiet lap defaul gateway cho Router ##########"
+echo "########## SETUP DEFAULT GATEWAY FOR ROUTER ##########"
 sleep 3
 neutron router-gateway-set router_1 ext_net
 
-echo "########## Khai bao network cua tenant cho Router ##########"
+echo "########## ASSIGN TENANT'S NETWORK FOR ROUTER ##########"
 sleep 3
 neutron router-interface-add router_1 int_subnet
 
@@ -54,10 +54,10 @@ ID_int_net=`neutron net-list | awk '/int*/ {print $2}'`
 # echo $ID_int_net
 
 
-echo "########## Tao may ao ten la vm6969 de kiem tra ##########"
+echo "########## CREATE VM NAME vm6969 FOR TESTING ##########"
 nova boot vm6969 --image cirros-0.3.2-x86_64 --flavor 1 --security-groups default --nic net-id=$ID_int_net
 
-echo "########## Hoan thanh viec tao may ao ##########"
+echo "########## CHECKING .. ##########"
 sleep 10
 nova list 
 # Tao flavor or example, create a new flavor called m1.custom with an ID of 6, 512 MB of RAM, 5 GB of root disk space, and 1 vCPU:
